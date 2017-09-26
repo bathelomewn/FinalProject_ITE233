e8130c1326315377c23a8356d1cd1d225089ea68
ITE233 <bathelomew.nwazodo@students.stamford.edu>
2017-09-26 09:01:20 +0700

    The Cramper Project: includes Homepage,  CraperRegister, CraperRegisterActivities and CraperSectiveActivity. The HomePage is done already

---
+++ ICAMP_Project/HomePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICAMP_Project
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String namestr = comboBox1.Text;
+            comboBox1.Items.Add(namestr);
+
+            //comboBox1.Items.Clear();
+
+        }
+    }
+}
