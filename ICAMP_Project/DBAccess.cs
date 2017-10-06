020e35eee8e9a805b52eb5c0b7d258b1a09a57b6
Tafadzwa Makuyana <tafadzwa.makuyana@students.stamford.edu>
2017-10-06 09:25:27 +0700

    dabase Connection Code, connection to local host

--- ICAMP_Project/DBAccess.cs
+++ ICAMP_Project/DBAccess.cs
@@ -9,6 +9,31 @@ namespace ICAMP_Project
 {
     class DBAccess
     {
- 
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBAccess()
+        {
+            initConnection();
+        }
+
+        public MySqlConnection initConnection()
+        {
+
+            server = "localhost";
+            database = "icamp";
+            uid = "root";
+            password = "";
+
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            return connection;
+
+        }
     }
 }
